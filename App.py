295d16521daa8310960f165f8a23ac74d623f805
Hardik Arora <hardikarora483@gmail.com>
2025-01-02 18:16:03 +0530

    first commit

---
+++ App.py
@@ -0,0 +1,18 @@
+# User will be able to Register in the website 
+# User will recieve a confirmation email in order to activate their account
+
+''''
+Commands  :
+virtual enviornment : 
+    1. pip install virtualenv
+    2. python -m virtualenv venv(name_of_venv)
+    3. venv\Scripts\activate
+django project : 
+    1. Django Project : django-admin startproject project_name
+    2. Django App : python manage.py startapp app_name
+    3. Superuser(Admin) : python manage.py createsuperuser
+    Username : hardikarora121
+    emailAddress : hardikarora483@gmail.com
+    password : 121
+
+'''
---
+++ logsys/logsys/info.py
@@ -0,0 +1,6 @@
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = "hardikarora483@gmail.com"
+EMAIL_HOST_PASSWORD = "nhiv cqzu wjxa zdri"
+EMAIL_PORT = 587
+
